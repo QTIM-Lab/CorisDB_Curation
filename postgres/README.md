e79a929fee01aad07e165af034efda878753af9f
bbearce <bbearce@gmail.com>
2023-04-06 13:32:13 -0600

    adding postgres folder with scripts

---
+++ postgres/README.md
@@ -0,0 +1,48 @@
+# CORIS DB
+
+## DB
+```sql
+-- Create a new DB
+CREATE DATABASE coris_db;
+
+-- Alter database owner
+ALTER DATABASE coris_db owner to ophuser;
+
+/* Misc server side edits and commands
+-- edited: sudo vim /etc/postgresql/15/main/pg_hba.conf to add ophuser to coris_db on line 93.
+-- ran: sudo service postgresql restart
+-- login
+psql -U ophuser coris_db
+
+/*
+```
+
+## Source data (SOURCE)
+* John Finigan moved the data to my home dir: /home/bearceb. I need to move it to it's main spot /data/coris_db
+* sudo ls /data/coris_db gives permission denied so we can do that later.
+* There is a problem with the files natively where I think line endings somewhere are "CRLF" vs "LF" which is a Windows\Linux,Mac config.
+
+## Code
+Code for table loading and scheme creation located here: /projects/coris_db currently /projects/CORIS_DB
+
+* initial_psql_scripts.sql
+  - creates database coris_db
+  - changes owner
+  - makes config edits to allow ophuser to login to coris_db
+* SOURCE Registry Local Users.xlsx - database scheme
+* table_def_orig_csvs
+  - csvs from above file representing the data found in the tabs
+  - create_tables.sql 
+* convert_types_and_create_data.py
+  - Uses table_def_orig_csvs to make the create_tables.sql script
+  - Creates sample data for test import at sample_data
+* make_postgres_importable_files.py
+  - CRLF versus LF discrepancies causeing import errors. Will make temporary version of files that can be imported.
+  - This will be in folder "tmp_for_import"
+* sample_data
+  - sample csvs for inmport
+
+Project dependency management:
+* pyproject.toml
+* pdm.lock
+* .pdm.toml 
---
+++ postgres/make_postgres_importable_files.py
@@ -0,0 +1,75 @@
+import os, shutil, pdb, pandas as pd
+
+SOURCE_directory="/home/bearceb/SOURCE"
+tmp_for_import="/projects/CORIS_DB/tmp_for_import"
+table_def_orig_csvs="table_def_orig_csvs"
+
+type_key={
+    "INTEGER":"Int64",
+    "STRING":"object",
+    "LONG_STRING":"object",
+    "DATETIME":"datetime64",
+    "TIMESTAMP":"datetime64",
+    "DATE":"datetime64",
+    "FLOAT":"float64",
+}
+
+
+# Make directory if doesn't exists
+if not os.path.isdir(tmp_for_import):
+    os.mkdir(tmp_for_import)
+
+# Read in files and write again via pandas to tmp directory
+# for csv in [i for i in os.listdir(SOURCE_directory) if i.find(".csv") != -1]:
+
+# Above for loop explicit
+csvs = [
+        # Done commented
+        # "OphthalmologyCurrentMedications_20230217.csv",
+        # "OphthalmologyDiagnosesDm_20230222.csv",
+        # "OphthalmologyEncounterCharge_20230217.csv",
+        # "OphthalmologyEncounterDiagnoses_20230217.csv",
+        # "OphthalmologyEncounterExam_20230217.csv",
+        # "OphthalmologyEncounterProblemList_20230217.csv",
+        # "OphthalmologyEncounters_20230217.csv",
+        # "OphthalmologyEncounterVisit_20230217.csv",
+        # "OphthalmologyFamilyHistory_20230217.csv",
+        # "OphthalmologyImplant_20230217.csv",
+        # "OphthalmologyImplantDm_20230222.csv",
+        # "OphthalmologyLabOrder_20230217.csv",
+        # "OphthalmologyLabs_20230217.csv",
+        # "OphthalmologyMedicationDm_20230222.csv",
+        # "OphthalmologyMedications_20230217.csv",
+        # "OphthalmologyOrders_20230217.csv",
+        # "OphthalmologyPatientDiagnoses_20230217.csv",
+        "OphthalmologyPatients_20230217.csv",
+        # "OphthalmologyProviderAll_20230217.csv",
+        # "OphthalmologyProviderDm_20230222.csv",
+        # "OphthalmologyRadiology_20230217.csv",
+        # "OphthalmologySupply_20230217.csv",
+        # "OphthalmologySupplyDm_20230222.csv",
+        # "OphthalmologySurgery_20230217.csv",
+        # "OphthalmologySurgeryAll_20230217.csv",
+        # "OphthalmologySurgeryBill_20230217.csv",
+        # "OphthalmologySurgeryDm_20230222.csv",
+        # "OphthalmologySurgeryMedication_20230217.csv",
+        # "OphthalmologySurgeryProcedure_20230217.csv",
+        # "OphthalmologySurgerySurgeon_20230217.csv",
+        # "OphthalmologyVisitSummary_20230217.csv",      
+        ]
+for csv in csvs:
+    print(csv)
+    # pdb.set_trace()
+    types = pd.read_csv(os.path.join(table_def_orig_csvs, csv.split("_")[0]+".csv"))
+    types['Type_pandas'] = types['Type'].apply(lambda x: type_key[x])
+    new_types = dict(zip(types['Field name'], types['Type_pandas']))
+    tmp = pd.read_csv(os.path.join(SOURCE_directory, csv), dtype=object)
+    tmp = tmp.fillna("NULL")
+    # tmp.astype(new_types)
+    # tmp['TOTAL_ENC'].astype('Int64')
+    tmp.to_csv(os.path.join(tmp_for_import, csv), index=None, quotechar="'", lineterminator="\n")
+
+
+# Delete directory if exists
+# if os.path.isdir(tmp_for_import):
+#     shutil.rmtree(tmp_for_import)
