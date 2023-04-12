a5a32935c251368642903c8ceefb3f8036c8ae21
bbearce <bbearce@gmail.com>
2023-04-12 13:36:39 -0600

    CORS and .env

--- flask/app.py
+++ flask/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 import psycopg, json, json, datetime
 
 app = Flask(__name__)
+CORS(app)
 
 DB = [{'name': 'Alice', 
       'email': 'alice@outlook.com'},
@@ -10,15 +12,18 @@ DB = [{'name': 'Alice',
 
 # DB talking to postgres
 host='0.0.0.0'
-conn = psycopg.connect(dbname='', user='', password='$$', host=host, port='')
-
+conn = psycopg.connect(dbname='coris_db', user='ophuser', password='OOOppphhhP4$$', host=host, port='5432')
 
 
 @app.route('/api')
-def api():
+def test():
     return jsonify(DB[0])
 
-@app.route('/api_postgres')
+@app.route('/api/connection_details')
+def connection_details():
+    return jsonify(DB[0])
+
+@app.route('/api/postgres')
 def api_postgres():
     # Open a cursor to perform database operations
     with conn.cursor() as cur:
@@ -28,6 +33,7 @@ def api_postgres():
 
     data_row = data[0]
     date_row_prepped = [i if type(i) != datetime.datetime else i.year for i in data_row]
+    # json.dumps(date_row_prepped)
     return_json = json.dumps(date_row_prepped)
 
     return return_json
