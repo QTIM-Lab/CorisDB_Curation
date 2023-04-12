e202a450347bc0cf9b98ac2b7d421be1adc6d7cd
bbearce <bbearce@gmail.com>
2023-04-12 13:38:44 -0600

    vue changes for glaucoma

---
+++ vue/project/src/components/Glaucoma.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-container>
+    {{message}}
+
+  </v-container>
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      message: 'Welcome to the Glaucoma Page'
+    }
+  },
+  mounted() {
+    console.log('Component mounted.')
+    this.test()
+  },
+  methods: {
+    test: () => {
+      console.log("Getting Glaucoma Data")
+      const url = "http://0.0.0.0:38147/api/connection_details"
+      debugger
+      console.log(url)
+      fetch(url)
+        .then(response => response.json())
+        .then(data => {
+          debugger;
+          // Do something with the returned data
+        })
+        .catch(error => {
+          // Handle any errors that occurred during the fetch request
+        });
+    }
+  }
+}
+</script>
