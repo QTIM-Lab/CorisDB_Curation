ab73613ba3b8ea29f977eee8e5d1cdeb245381e7
bbearce <bbearce@gmail.com>
2023-09-27 12:42:58 -0600

    09_27_2023_migration

--- vue/project/src/router/index.ts
+++ vue/project/src/router/index.ts
@@ -23,12 +23,52 @@ const routes = [
         component: () => import(/* webpackChunkName: "coris_db" */ '@/views/Coris_DB.vue'),
         children: [
           {
+            path: 'amd',
+            name: 'amd',
+            // route level code-splitting
+            // this generates a separate chunk (about.[hash].js) for this route
+            // which is lazy-loaded when the route is visited.
+            component: () => import(/* webpackChunkName: "amd" */ '@/views/AMD.vue'),
+          },
+          {
+            path: 'glaucoma_click',
+            name: 'glaucoma_click',
+            // route level code-splitting
+            // this generates a separate chunk (about.[hash].js) for this route
+            // which is lazy-loaded when the route is visited.
+            component: () => import(/* webpackChunkName: "glaucoma_click" */ '@/views/GlaucomaClick.vue'),
+          },
+          {
+            path: 'glaucoma_hover',
+            name: 'glaucoma_hover',
+            // route level code-splitting
+            // this generates a separate chunk (about.[hash].js) for this route
+            // which is lazy-loaded when the route is visited.
+            component: () => import(/* webpackChunkName: "glaucoma_hover" */ '@/views/GlaucomaHover.vue'),
+          },
+          {
+            path: 'glaucoma_bb',
+            name: 'glaucoma_bb',
+            // route level code-splitting
+            // this generates a separate chunk (about.[hash].js) for this route
+            // which is lazy-loaded when the route is visited.
+            component: () => import(/* webpackChunkName: "glaucoma_bb" */ '@/views/GlaucomaBB.vue'),
+          },
+          {
             path: 'glaucoma',
             name: 'glaucoma',
             // route level code-splitting
             // this generates a separate chunk (about.[hash].js) for this route
             // which is lazy-loaded when the route is visited.
             component: () => import(/* webpackChunkName: "glaucoma" */ '@/views/Glaucoma.vue'),
+          },
+          {
+            path: 'glaucoma_scott',
+            name: 'glaucoma_scott',
+            // route level code-splitting
+            // this generates a separate chunk (about.[hash].js) for this route
+            // which is lazy-loaded when the route is visited.
+            component: () => import(/* webpackChunkName: "glaucoma_scott" */ '@/views/GlaucomaScott.vue'),
           },
           // {
           //   path: 'amd',
---
+++ vue/project/src/views/GlaucomaBB.vue
@@ -0,0 +1,7 @@
+<template>
+  <GlaucomaBB ssh-port-forward="34851"/>
+</template>
+
+<script lang="ts" setup>
+  import GlaucomaBB from '@/components/GlaucomaBB.vue'
+</script>
