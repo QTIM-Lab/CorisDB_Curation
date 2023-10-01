49ea7e6f74fde438c2e1d3706480f56fa74f4165
Benjamin Bearce <bbearce@gmail.com>
2023-09-30 18:58:07 -0600

    update

--- vue/project/src/views/Patient.vue
+++ vue/project/src/views/Patient.vue
@@ -1,35 +1,26 @@
 <template>
-    <v-alert
-      v-model="showAlert"
-      title="Alert title"
-      text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore voluptatibus! Eaque cupiditate minima, at placeat totam, magni doloremque veniam neque porro libero rerum unde voluptatem!"
-    ></v-alert>
-
     <v-container fluid class="rounded elevation-1">
         <v-row class="rounded elevation-1">
-        <v-col cols="5" class="rounded elevation-1">
-            <p>cols="5"</p>
-        </v-col>
-        <v-col cols="2" v-if="true" class="rounded elevation-1">
-            <p>cols="2"</p>
-            <v-label>Name:</v-label>
-            <p>SAMPLE</p>
-        </v-col>
-        <v-col cols="1" v-if="true" class="rounded elevation-1">
-            <p>cols="1"</p>
-            <v-label>MRN</v-label>
-            <p>123456</p>
-        </v-col>
-        <v-col cols="1" v-if="true" class="rounded elevation-1">
-            <p>cols="1"</p>
-            <v-label>Age</v-label>
-            <p>50</p>
-        </v-col>
-        <v-col cols="1" v-if="true" class="rounded elevation-1">
-            <p>cols="1"</p>
-            <v-label>Gender</v-label>
-            <p>F</p>
-        </v-col>
+            <v-col cols="5" class="rounded elevation-1">
+                <p>cols="5"</p>
+                <v-text-field
+                    label="Look up Patient in Database (Not active..yet)">
+                </v-text-field>
+                <v-autocomplete 
+                    label="Recent Patients"
+                    v-model="selectedPatientAutocomplete" 
+                    @change="searchPatients"
+                    @keydown="searchPatients"
+                    :items="patients.map((patient) => patient.name + ' (' + patient.mrn + ')')" 
+                >
+                <!-- https://github.com/vuetifyjs/vuetify/issues/12880 -->
+                </v-autocomplete>
+            </v-col>
+            <v-col cols="7" class="rounded elevation-1">
+                <p>cols="7"</p>
+                <PatientDemographics></PatientDemographics>
+                <EncounterSlider></EncounterSlider>
+            </v-col>
         </v-row>
 
         <v-row class="rounded elevation-1">
@@ -37,23 +28,28 @@
                 <p>cols="3"</p>
                 <EpicFlowSheetData></EpicFlowSheetData>
             </v-col>
+            <!-- CarouselDeck -->
             <v-col cols="9" class="rounded elevation-1">
                 <p>cols="9"</p>
                 <v-row class="rounded elevation-1">
                 <v-col cols="6" class="rounded elevation-1">
                     <p>cols="6"</p>
+                    <ImageComponent></ImageComponent>
                 </v-col>
                 <v-col cols="6" class="rounded elevation-1">
                     <p>cols="6"</p>
+                    <ImageComponent></ImageComponent>
                 </v-col>
                 </v-row>
                 <v-row class="rounded elevation-1">
                 <v-col cols="6" class="rounded elevation-1">
                     <p>cols="6"</p>
+                    <ImageComponent></ImageComponent>
                 </v-col>
 
                 <v-col cols="6" class="rounded elevation-1">
                     <p>cols="6"</p>
+                    <ImageComponent></ImageComponent>
                 </v-col>
                 </v-row>
             </v-col>
@@ -66,13 +62,29 @@
   
 <script lang="ts">
 import EpicFlowSheetData from '@/components/General_Components/EpicFlowSheetData.vue'
-import TemplateComponent from '@/components/General_Components/TemplateComponent.vue'
+import PatientDemographics from '@/components/General_Components/PatientDemographics.vue'
+import ImageComponent from '@/components/Image_Modalities/ImageComponent.vue'
+import EncounterSlider from '@/components/General_Components/EncounterSlider.vue'
 
 export default {
     data: () => ({
-        showAlert: true,
+        selectedPatientAutocomplete: '',
+        patients: [
+        {
+          "mrn": 999999,
+          "name": "PersonA",
+          "age": 74,
+          "gender": "Female"
+        },
+        {
+          "mrn": 444444,
+          "name": "PersonB",
+          "age": 64,
+          "gender": "Male"
+        },
+      ],
     }),
-    components: { EpicFlowSheetData },
+    components: { EpicFlowSheetData, PatientDemographics, ImageComponent, EncounterSlider },
     mounted() {
         console.log("Patient mounted!");
         this.test();
@@ -81,8 +93,34 @@ export default {
         test: function () {
         console.log("Patient view loaded successfully!");
         },
-        toggleAlert() {
-        this.showAlert = !this.showAlert;
+        searchPatients() {
+            var inputString = this.selectedPatientAutocomplete;
+            if (!inputString) { // check if inputString is null or undefined
+                alert('Input string is empty or null.');
+                return;
+            }
+            const regex = /^(.*?)\s+\((\d+)\)$/; 
+            var match = inputString.match(regex);
+            if (!match) { // check if match result is null
+            alert('No match found.');
+            return;
+            }
+            var name = match[1]; // Captured group 1 (.*?)
+            var mrn: number | null = parseInt(match[2]); // Captured group 2 (\d+)
+            
+            var foundPatient = this.patients.find((patient) => patient.mrn === mrn);
+            if (!foundPatient) {
+            alert('No patient found.');
+            return;
+            }
+            this.selectedPatient = foundPatient;
+            
+            var foundPatientImages = this.patientImages.find((imageSet) => imageSet.mrn === mrn)
+            if (!foundPatientImages) {
+            alert('No patient images found.');
+            return;
+            }
+            this.imageSet = foundPatientImages;
         },
     },
 };
